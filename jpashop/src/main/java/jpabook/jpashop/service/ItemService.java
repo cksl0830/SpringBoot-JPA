7634e83e7db4745704f62662b59d01e273b6d8a1
Park Chan <60590737+cksl0830@users.noreply.github.com>
2022-04-13 14:21:40 +0900

    상품 등록 및 조회 & 수정

--- jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
+++ jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -20,6 +21,16 @@ public class ItemService {
         itemRepository.save(item);
     }
 
+    /**
+     변경 감지 :: 원하는 속성만 선택해서 변경가능 (영속성 컨텍스트에서 엔티티 다시조회 -> 데이터 수정)
+     merge 방법 :: 모든 속성 변경!! (값이 없으면 null로 업데이트)
+                  준영속 상태의 엔티티를 영속상태로 변경 해버림
+     결론 :: 트랜잭션 안에서 엔티티를 조회해야 영속상태로 조회 -> 여기서 변경해야 변경감지 일어남 (commit 할 때)
+        -> 데이터베이스에 업데이트 실행 :: 즉, 변경감지를 사용하자!!!!
+     */
+
+    // Setter 사용하지 말고 메서드 만들어서 역추적 가능하게 짜자!!!
+    // 추가적으로 넘길 파라미터가 많으면, DTO 생성해서 넘기면 됨
     @Transactional
     public void updateItem(Long itemId, String name, int price, int stockQuantity) {
         Item item = itemRepository.findOne(itemId);
